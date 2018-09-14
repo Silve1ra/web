eba81fb2e70216195de58ae6af7d090722723b8a
Unknown <eng.fe.silveira@gmail.com>
2018-09-14 12:49:16 -0400

    Começando a implementação do jogo da velha

---
+++ 05 - Document Object Model DOM/03 - myscript.js
@@ -0,0 +1,11 @@
+console.log("connected")
+
+//restart game button
+
+//grabs all the squares
+
+//clear all the squares
+
+//check the square marker
+
+//for loop to add event listeners to all the squares
