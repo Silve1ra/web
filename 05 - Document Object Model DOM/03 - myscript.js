1c17321c0f8779ff4a4542af05df6b7fb16fd2fa
Unknown <eng.fe.silveira@gmail.com>
2018-09-14 13:57:24 -0400

    Finalizando o jogo da velha

--- 05 - Document Object Model DOM/03 - myscript.js
+++ 05 - Document Object Model DOM/03 - myscript.js
@@ -1,11 +1,50 @@
-console.log("connected")
+//console.log("connected")
 
 //restart game button
+var restart = document.querySelector('#b')
 
 //grabs all the squares
+var squares = document.querySelectorAll('td')
 
 //clear all the squares
+function clearBoard(){
+    for(var i=0; i<squares.length; i++){
+        squares[i].textContent = ''
+    }
+}
+
+restart.addEventListener('click', clearBoard)
 
 //check the square marker
+
+/*
+jeito easy
+var cellOne = document.querySelector('#one')
+
+cellOne.addEventListener('click', function(){
+    if (cellOne.textContent ===''){
+        cellOne.textContent = 'X'
+    }else if(cellOne.textContent === 'X'){
+        cellOne.textContent='O'
+    }else{
+        cellOne.textContent=''
+    }
+})
+*/
+
+/* jeito hard */
+function changeMarker(){
+    if(this.textContent === ''){
+        this.textContent = 'X'
+    }else if (this.textContent === 'X'){
+        this.textContent = 'O'
+    }else{
+        this.textContent = ''
+    }
+}
+
+for (var i=0; i<squares.length; i++){
+    squares[i].addEventListener('click', changeMarker)
+}
 
 //for loop to add event listeners to all the squares
